a9bc66205297c507a29e5bf791773f8d272ab40c
zoleg-oi <zoleg@mail.ru>
2024-08-05 18:54:56 +0300

    Add files via upload

---
+++ main_base_type.py
@@ -0,0 +1,27 @@
+# Это практическое задание к у року "Базовые структуры данных"
+
+# "1st programm"
+print('"1st programm"')
+print(9**0.5*5)
+print()
+# "2nd programm"
+print('"2nd programm"')
+print(9.99>9.98 and 1000 != 100.1)
+print()
+# "3rd program"
+print('"3rd program"')
+
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2) == (2*(2+2)))
+print()
+# "4th program"
+print('"4th program"')
+str_a = '123.456'
+print(str_a[4])
+flo_a = float(str_a)
+flo_a = flo_a * 10
+int_a = int(flo_a)
+print('Это формат float - ' + str(flo_a) , 'Это формат integer - ' + str(int_a), sep= '\n' )
+print('Это результат - ' + str(int_a%10))
+
